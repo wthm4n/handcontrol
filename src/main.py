f9399db5cbffd186b828cb97051fc3e95050c77b
wthm4n <noenoeke11@gmail.com>
2026-06-22 15:22:58 +0530

    chore(main): update src/main.py

--- src/main.py
+++ src/main.py
@@ -6,7 +6,7 @@ import sys
 
 
 CAMERA_DEVICE = "/dev/video10"
-cap = cv2.VideoCapture(10, cv2.CAP_V4L2)
+cap = cv2.VideoCapture(CAMERA_DEVICE)
 
 if not cap.isOpened():
     print(f"Error: Could not open camera {CAMERA_DEVICE}. Exiting.")
